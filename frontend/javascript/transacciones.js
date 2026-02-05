4a28446ca41f7d58bd0a5a9a755c6d430828892f
Estrella Pallarta <epallarta@fi.uba.ar>
2026-02-05 18:57:06 -0300

    hago lo mismo con los demas botones

--- frontend/javascript/transacciones.js
+++ frontend/javascript/transacciones.js
@@ -1,6 +1,21 @@
 const botonCrearTransaccion = document.querySelector("#nueva-transaccion");
 const modal = document.querySelector("#modal-transaccion");
+const botonCerrar = document.querySelector("#cerrar-modal");
+const botonCancelar = document.querySelector("#cancelar-form");
+const botonGuardar = document.querySelector("#guardar-form");
 
-botonCrearTransaccion.addEventListener("click", ()=> {
+botonCrearTransaccion.addEventListener("click", (e)=> {
     modal.classList.add("is-active");
+})
+
+botonCerrar.addEventListener("click", (e)=> {
+    modal.classList.remove("is-active");
+})
+
+botonCancelar.addEventListener("click", (e)=> {
+    modal.classList.remove("is-active");
+})
+
+botonGuardar.addEventListener("click", (e)=> {
+    modal.classList.remove("is-active");
 })
