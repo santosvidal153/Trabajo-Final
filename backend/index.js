5c84a9b83876b9497fe7132cd11d481dc681c61e
Estrella Pallarta <epallarta@fi.uba.ar>
2026-02-05 17:42:57 -0300

    muevo pool a otro archivo y creo nuevo archivo para guardar el back de transacciones

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,19 @@
 import express from "express";
-import pg from "pg";
 import dotenv from "dotenv";
 
-//agregar archivos desde acá
+//agregar routes desde acá
 
 
 dotenv.config();
-
-const { Pool } = pg;
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
 const app = express();
+app.use(express.json());
 
 app.get("/",(req, res) => {
     res.send("backend ok");
 });
+
+//agregar endpoints desde acá
+
 
 app.listen(process.env.PORT, () => {
   console.log("backend ok, puerto:", process.env.PORT);
---
+++ backend/routes/transacciones-back.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { pool } from "./db.js";
+
+const router = express.Router();
+
+export default router;
