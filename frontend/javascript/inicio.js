4f74e2dd6b76b5650ec2b8eba281c726221734ed
Estrella Pallarta <epallarta@fi.uba.ar>
2026-02-10 20:12:29 -0300

    agrego porcentajes y diseños de notificacion

--- frontend/javascript/inicio.js
+++ frontend/javascript/inicio.js
@@ -33,6 +33,7 @@ const resumenMensual = async () => {
         const gastoMensual = gastos.reduce((a,b) => a + Number(b.monto), 0);
         const ahorroMensual = ahorros.reduce((a,b) => a + Number(b.monto), 0);
 
+        //resumen mensual
         const filaIngreso = document.querySelector("#ingreso-mensual");
         filaIngreso.textContent = `$${ingresoMensual}`
 
@@ -45,6 +46,36 @@ const resumenMensual = async () => {
         const saldoMensual = ingresoMensual - gastoMensual - ahorroMensual;
         const filaSaldo = document.querySelector("#saldo-mensual")
         filaSaldo.textContent = `$${saldoMensual}`
+
+        const porcDisp = Number((saldoMensual * 100 / ingresoMensual).toFixed(2));
+        const porcentaje = document.querySelector("#nota-porcentaje");
+
+        //notificacion de abajo
+        if (porcDisp <= 100 && porcDisp > 60) {
+            porcentaje.innerHTML = `
+            <article class="message is-success">
+            <div class="message-body">
+              ¡Excelente! Te queda el ${porcDisp}% de tu ingreso disponible este mes. 
+            </div>
+            </article>`
+        }
+        if (porcDisp <= 60 && porcDisp > 30) {
+            porcentaje.innerHTML = `
+            <article class="message is-warning">
+            <div class="message-body">
+              ¡Muy bien! Te queda el ${porcDisp}% de tu ingreso disponible este mes. 
+            </div>
+            </article>`
+        }
+        if (porcDisp <= 30 && porcDisp > 0) {
+            porcentaje.innerHTML = `
+            <article class="message is-danger">
+            <div class="message-body">
+              ¡Cuidado! Te queda el ${porcDisp}% de tu ingreso disponible este mes. 
+            </div>
+            </article>`
+        }
+        
     }
     catch (err) {
         console.error;
