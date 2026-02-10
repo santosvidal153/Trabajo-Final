3f37416b953e543a01c8617a2676f50bbb917b60
Estrella Pallarta <epallarta@fi.uba.ar>
2026-02-10 19:43:48 -0300

    agrego resumen mensual a inicio

--- frontend/javascript/inicio.js
+++ frontend/javascript/inicio.js
@@ -19,8 +19,43 @@ const mostrarBienvenida = async () => {
     }
 }
 
+const resumenMensual = async () => {
+    try {
+        const url =  `http://localhost:3000/usuario/${usuarioId}/inicio`
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const ingresos = data.filter( item => item.tipo === "ingreso");
+        const gastos = data.filter(item => item.tipo === "gasto");
+        const ahorros = ingresos.filter( item => item.categoria === "ahorro");
+
+        const ingresoMensual = ingresos.reduce((a,b) => a + Number(b.monto), 0)
+        const gastoMensual = gastos.reduce((a,b) => a + Number(b.monto), 0);
+        const ahorroMensual = ahorros.reduce((a,b) => a + Number(b.monto), 0);
+
+        const filaIngreso = document.querySelector("#ingreso-mensual");
+        filaIngreso.textContent = `$${ingresoMensual}`
+
+        const filaGasto = document.querySelector("#gasto-mensual");
+        filaGasto.textContent = `$${gastoMensual}`
+
+        const filaAhorro = document.querySelector("#ahorro-mensual");
+        filaAhorro.textContent = `$${ahorroMensual}`
+
+        const saldoMensual = ingresoMensual - gastoMensual - ahorroMensual;
+        const filaSaldo = document.querySelector("#saldo-mensual")
+        filaSaldo.textContent = `$${saldoMensual}`
+    }
+    catch (err) {
+        console.error;
+        alert(err.message);
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", ()=> {
     mostrarBienvenida();
+    resumenMensual();
 })
 
 
