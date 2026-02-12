e28cc2a8d19587da9360c7fd0e251c2be7a30559
Valentina <valentinamartel05@gmail.com>
2026-02-12 09:53:17 -0300

    Importo archivo objetivos a index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/objetivos', objetivosRouter);
+
 app.get("/",(req, res) => {
     res.send("backend ok");
 });
